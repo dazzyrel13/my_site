748797ac303ca6956aa33ac638674cacd5abe6eb
Dazzyrel13 <dazzyrel@gmail.com>
2026-02-26 11:00:25 +0900

    Улучшение UX: предупреждение о цене теперь не показывается на странице контактов и на небольших ценах

---
+++ .temp_git_commit.bat
@@ -0,0 +1,5 @@
+@echo off
+cd /d "c:\Users\KDFX Modes\Desktop\my_site"
+git add templates/base.html
+git commit -m "Улучшение UX: предупреждение о цене теперь не показывается на странице контактов и на небольших ценах"
+git push
